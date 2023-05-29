695ababb08ed20319d9c5ede198623113756957c
Nico <edenilson.passos@yahoo.com>
2023-05-29 15:21:12 -0300

    Update profile.py

--- profile.py
+++ profile.py
@@ -20,31 +20,36 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 # Node ryu
-node_ryu = request.RawPC('ryu')
+node_ryu = request.XenVM('ryu')
 node_ryu.routable_control_ip = True
+node_ryu.Site('Site 1')
 
 # Node ovs
-node_ovs = request.RawPC('ovs')
+node_ovs = request.XenVM('ovs')
 node_ovs.routable_control_ip = True
+node_ovs.Site('Site 2')
 iface0 = node_ovs.addInterface('interface-6')
 iface1 = node_ovs.addInterface('interface-8')
 iface2 = node_ovs.addInterface('interface-10')
 
 # Node h1
-node_h1 = request.RawPC('h1')
+node_h1 = request.XenVM('h1')
 node_h1.routable_control_ip = True
+node_h1.Site('Site 2')
 node_h1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
 iface3 = node_h1.addInterface('interface-11')
 
 # Node h3
-node_h3 = request.RawPC('h3')
+node_h3 = request.XenVM('h3')
 node_h3.routable_control_ip = True
+node_h3.Site('Site 2')
 node_h3.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
 iface4 = node_h3.addInterface('interface-7')
 
 # Node h2
-node_h2 = request.RawPC('h2')
+node_h2 = request.XenVM('h2')
 node_h2.routable_control_ip = True
+node_h2.Site('Site 2')
 node_h2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
 iface5 = node_h2.addInterface('interface-9')
 
@@ -67,9 +72,10 @@ link_5.addInterface(iface2)
 link_5.addInterface(iface3)
 
 # Install and execute a script that is contained in the repository.
-node_h1.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-node_h2.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-node_h3.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node_h1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node_h2.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node_h3.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
+node_h1.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install gedit"))
 
 # Print the generated rspec
 pc.printRequestRSpec(request)
