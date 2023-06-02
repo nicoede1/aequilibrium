6464ecb7b1db8a5a7655a625974b5a5b21f8212d
Nico <edenilson.passos@yahoo.com>
2023-06-02 15:33:30 -0300

    Update redirecttcp.py

--- redirecttcp.py
+++ redirecttcp.py
@@ -2,14 +2,15 @@ from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto import ofproto_v1_0
+from ryu.lib.packet import ether_types, in_proto, ipv4, tcp
 
-class SimpleLoadBalancer(app_manager.RyuApp):
-    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+class PacketRedirector(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleLoadBalancer, self).__init__(*args, **kwargs)
-        self.mac_to_port = {}
+        super(PacketRedirector, self).__init__(*args, **kwargs)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -17,19 +18,22 @@ class SimpleLoadBalancer(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # Install the flow rules to redirect packets
-        match = parser.OFPMatch(eth_type=0x0800, ip_proto=6, tcp_dst=80, ipv4_dst='10.10.1.2')
-        actions = [
-            parser.OFPActionSetField(ipv4_dst='10.10.1.3'),
-            parser.OFPActionSetField(ipv4_src='10.10.1.3'),
-            parser.OFPActionOutput(ofproto.OFPP_NORMAL)
-        ]
+        # Install a flow rule to redirect HTTP packets
+        match = parser.OFPMatch(dl_type=ether_types.ETH_TYPE_IP,
+                               nw_proto=in_proto.IPPROTO_TCP,
+                               tp_dst=80,
+                               eth_dst='02:90:90:fc:7b:5a',  # Destination MAC
+                               ipv4_dst='10.10.1.2')  # Destination IP
+        actions = [parser.OFPActionSetField(ipv4_dst='10.10.1.3'),
+                   parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
         self.add_flow(datapath, 100, match, actions)
 
     def add_flow(self, datapath, priority, match, actions):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
         datapath.send_msg(mod)
