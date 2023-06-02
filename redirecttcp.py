a1a44d7dc0a543b5ce2bc559aa739c7c93d8391a
Nico <edenilson.passos@yahoo.com>
2023-06-02 18:27:07 -0300

    Update redirecttcp.py

--- redirecttcp.py
+++ redirecttcp.py
@@ -1,36 +1,41 @@
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 
-class MyController(app_manager.RyuApp):
+class PacketRedirector(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(MyController, self).__init__(*args, **kwargs)
+        super(PacketRedirector, self).__init__(*args, **kwargs)
 
-    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, MAIN_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # Add flow rule to redirect TCP traffic on port 80
-        match = parser.OFPMatch(eth_type=0x0800, ipv4_dst='10.10.1.2',
-                                ip_proto=6, tcp_dst=80)
-        actions = [parser.OFPActionSetField(ipv4_dst='10.10.1.3'),
-                   parser.OFPActionOutput(ofproto.OFPP_NORMAL)]
-        self.add_flow(datapath, 1, match, actions)
+        # Match packets from host 10.10.1.1 and TCP port 80
+        match = parser.OFPMatch(
+            eth_type=0x0800,  # IPv4
+            ipv4_src='10.10.1.1',
+            ip_proto=6,  # TCP
+            tcp_dst=80
+        )
 
-    def add_flow(self, datapath, priority, match, actions):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
+        # Set the new destination MAC address to redirect packets to 10.10.1.3
+        actions = [parser.OFPActionSetField(eth_dst='02:79:15:ab:5d:fc')]
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                match=match, instructions=inst)
+        # Apply the actions to the matched packets
+        instructions = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        # Add the flow entry to the switch's flow table
+        mod = parser.OFPFlowMod(
+            datapath=datapath,
+            priority=100,  # Set an appropriate priority
+            match=match,
+            instructions=instructions
+        )
         datapath.send_msg(mod)
-
-app_manager.require_app('ryu.app.simple_switch')
